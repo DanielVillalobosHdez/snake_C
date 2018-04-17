1a8272c1de45774c8892d95a8f1b076abb4b99c0
danielvillaloboshdez <danivillalobos1997@gmail.com>
2018-04-17 13:02:56 +0200

    new

--- snake.cpp
+++ snake.cpp
@@ -3,8 +3,31 @@
 #include <ncurses.h>
 
 
+struct Snake {
+    double x;
+    double y;
+
+};
+void begin(struct Snake go){
+    for (int i=0; i<N; i++){
+        go[i].y = LINE/2;
+        go[i].x = COLS / 2;
+    }
+}
 int main(){
+    int size = 3;
+    struct Snake body[i]{};
 
 
-  return EXIT_SUCCESS;
+    initsrc();
+    begin(body)
+    for (int i = 0 ;  i<size ; i++) {
+        mvprintw(body[i].x,body[i].y, "C");
+    }
+    refresh();
+    endwin();
+
+
+
+    return EXIT_SUCCESS;
 }
