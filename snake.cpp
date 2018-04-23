7d37100041f18df72a9f67f82eca17b89505bb07
davidgldv <davidgldv@gmail.com>
2018-04-23 12:31:26 -0400

    Fallo de Compilacion

--- snake.cpp
+++ snake.cpp
@@ -1,33 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <unistd.h>
+#include <time.h>
 
-
+#define N 4
+#define T 25 /*Maximo del cuerpo*/
 struct Snake {
     double x;
     double y;
 
 };
-void begin(struct Snake go){
-    for (int i=0; i<N; i++){
-        go[i].y = LINE/2;
-        go[i].x = COLS / 2;
-    }
-}
+void CalcularComienzo(struct Snake *body[T]);
+void Pintar(struct Snake body[T]);
+
 int main(){
     int size = 3;
-    struct Snake body[i]{};
+    struct Snake body[T];
+    struct Snake obj;
 
+    initscr();
+    curs_set(0);
+    CalcularComienzo(&body);
 
-    initsrc();
-    begin(body)
-    for (int i = 0 ;  i<size ; i++) {
-        mvprintw(body[i].x,body[i].y, "C");
+    while(1){ 
+	/*MoverCuerpo();
+     	*/
+	sleep(1);    
+    	Pintar(body);
+    	/*Comprobar();*/
+    
+    	refresh();
     }
-    refresh();
     endwin();
 
 
-
     return EXIT_SUCCESS;
+
+}
+void CalcularComienzo(struct Snake *body[T], int size){
+
+	int a = LINES/2;
+	int b = COLS /2;
+	for( int c ; c<size ; c++){
+	body[c]->y = a + c;
+        body[c]->x = b + c;
+	}
+}
+void Pintar(struct Snake body[T], int size){
+
+	for (int i = 0 ;  i<size ; i++) {
+        	mvprintw( (int) body[i].x,body[i].y, "C");
+    	} 
 }
