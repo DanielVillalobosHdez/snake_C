92043226945ccc768d3d276e0a4317cfc4909ed4
DanielVillalobosHdez <danivillalobos1997@gmail.com>
2018-04-26 18:41:17 +0200

    new

--- snake.cpp
+++ snake.cpp
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
 #include <unistd.h>
@@ -7,37 +6,48 @@
 #define N 4
 #define T 25 /*Maximo del cuerpo*/
 struct Snake {
-    double x;
-    double y;
+	double x;
+	double y;
 
 };
 void calcular_comienzo(struct Snake body[T], int size);
+void posicionar_objeto(struct Snake *obj);
 void pintar(struct Snake body[T], int size);
 void mover(struct Snake body[T], int size, struct Snake vel);
+void pintar_objeto(struct Snake *obj);
+void coger(struct Snake body[T], struct Snake *obj, int size);
 
 int main(){
-    int size = 3;
-    struct Snake body[T];
-    struct Snake obj;
-    struct Snake vel = {1., 0};
+	int size = 3;
+	struct Snake body[T];
+	struct Snake obj;
+	struct Snake vel = {1., 0};
+	int contador = 0;
 
-    initscr();
-    curs_set(0);
-    calcular_comienzo(body, size);
+	
+	srand(time(NULL));	
 
-    while(1){
+	initscr();
+	curs_set(0);
+	calcular_comienzo(body, size);
+	posicionar_objeto(&obj);
 
-        clear();
-        mover(body, size, vel);
-        pintar(body, size);
-    	/*Comprobar();*/
-    	refresh();
-        usleep(100000);
-    }
-    endwin();
+	while(1){
+		clear();
+		mover(body, size, vel);
+		pintar(body, size);
+		pintar_objeto(&obj);
+		if(body[0].y == obj.y && body[0].x == obj.x){
+                	size++;
+			posicionar_objeto(&obj);
+		}
+		refresh();
+		usleep(100000);
+	}
+	endwin();
 
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 
 }
 void calcular_comienzo(struct Snake body[T], int size){
@@ -45,24 +55,41 @@ void calcular_comienzo(struct Snake body[T], int size){
 	int a = LINES/2;
 	int b = COLS /2;
 	for( int c ; c<size ; c++){
-        body[c].y = a;
-        body[c].x = b + c;
+		body[c].y = a;
+		body[c].x = b + c;
 	}
 }
 void pintar(struct Snake body[T], int size){
 
 	for (int i = 0 ;  i<size ; i++)
-        	mvprintw((int) body[i].y, body[i].x, "0");
+		mvprintw((int) body[i].y, body[i].x, "0");
 
 }
 
 void mover(struct Snake body[T], int size, struct Snake vel){
-    for(int m = size-1; m > 0; m--){
-      body[m].x = body[m-1].x;
-      body[m].y = body[m-1].y;
-    }
+	for(int m = size-1; m > 0; m--){
+		body[m].x = body[m-1].x;
+		body[m].y = body[m-1].y;
+	}
 
-    body[0].x += vel.x;
-    body[0].y += vel.y;
+	body[0].x += vel.x;
+	body[0].y += vel.y;
+
+	if(body[0].x == 0 || body[0].y == 0 || body[0].x == COLS || body[0].y == LINES){
+		endwin();
+		exit(1);
+	}
+}
+void pintar_objeto(struct Snake *obj){
+
+	mvprintw(obj->y, obj->x, "*");
+}
+void coger(struct Snake body[T], struct Snake *obj, int size){
+	if(body[0].y == obj->y && body[0].x == obj->x)
+		size++;
 }
 
+void posicionar_objeto(struct Snake *obj){
+	obj -> x = rand() % COLS -1;
+        obj -> y = rand() % LINES -1 ;
+}
