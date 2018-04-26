f9227a406c04102fb10a6be43ba3147d3d905d5a
danielvillaloboshdez <danivillalobos1997@gmail.com>
2018-04-26 14:30:08 +0200

    new

--- snake.cpp
+++ snake.cpp
@@ -11,26 +11,28 @@ struct Snake {
     double y;
 
 };
-void CalcularComienzo(struct Snake *body[T]);
-void Pintar(struct Snake body[T]);
+void calcular_comienzo(struct Snake body[T], int size);
+void pintar(struct Snake body[T], int size);
+void mover(struct Snake body[T], int size, struct Snake vel);
 
 int main(){
     int size = 3;
     struct Snake body[T];
     struct Snake obj;
+    struct Snake vel = {1., 0};
 
     initscr();
     curs_set(0);
-    CalcularComienzo(&body);
+    calcular_comienzo(body, size);
 
-    while(1){ 
-	/*MoverCuerpo();
-     	*/
-	sleep(1);    
-    	Pintar(body);
+    while(1){
+
+        clear();
+        mover(body, size, vel);
+        pintar(body, size);
     	/*Comprobar();*/
-    
     	refresh();
+        usleep(100000);
     }
     endwin();
 
@@ -38,18 +40,29 @@ int main(){
     return EXIT_SUCCESS;
 
 }
-void CalcularComienzo(struct Snake *body[T], int size){
+void calcular_comienzo(struct Snake body[T], int size){
 
 	int a = LINES/2;
 	int b = COLS /2;
 	for( int c ; c<size ; c++){
-	body[c]->y = a + c;
-        body[c]->x = b + c;
+        body[c].y = a;
+        body[c].x = b + c;
 	}
 }
-void Pintar(struct Snake body[T], int size){
+void pintar(struct Snake body[T], int size){
 
-	for (int i = 0 ;  i<size ; i++) {
-        	mvprintw( (int) body[i].x,body[i].y, "C");
-    	} 
+	for (int i = 0 ;  i<size ; i++)
+        	mvprintw((int) body[i].y, body[i].x, "0");
+
 }
+
+void mover(struct Snake body[T], int size, struct Snake vel){
+    for(int m = size-1; m > 0; m--){
+      body[m].x = body[m-1].x;
+      body[m].y = body[m-1].y;
+    }
+
+    body[0].x += vel.x;
+    body[0].y += vel.y;
+}
+
